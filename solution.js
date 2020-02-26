1484f917b5cd025c67f34423ecb3dc9aefb5299a
Vhanni Sayo <bayanipsayoiii@gmail.com>
2020-02-26 21:05:54 +0800

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,8 +1,5 @@
-const run = require('./solution');
-const expect = require('expect');
+function myFunc(one, two) {
+  return one + two;
+}
 
-describe('Sample Exercise', () => {
-  it('should return the sum of two numbers', () => {
-    expect(run(1,2)).toEqual(3);
-  });
-});
+
